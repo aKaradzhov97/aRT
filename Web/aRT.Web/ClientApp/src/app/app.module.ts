1770a977163385838eeac617d2c04a677e4a3edf
aKaradzhov97 <amalcho97@gmail.com>
2020-05-02 22:48:09 +0300

    Added HTTP Interceptors, nxg loading bar & auth service.

--- Web/aRT.Web/ClientApp/src/app/app.module.ts
+++ Web/aRT.Web/ClientApp/src/app/app.module.ts
@@ -7,7 +7,8 @@ import {AppRoutingModule} from './app-routing.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {SharedModule} from './shared/shared.module';
 import {MaterialModule} from './modules/material/material.module';
-import {HttpClientModule} from '@angular/common/http';
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
+import { LoadingBarHttpClientModule } from '@ngx-loading-bar/http-client';
 import {EffectsModule} from '@ngrx/effects';
 import {MetaReducer, StoreModule} from '@ngrx/store';
 import {StoreDevtoolsModule} from '@ngrx/store-devtools';
@@ -15,6 +16,11 @@ import {StoreDevtoolsModule} from '@ngrx/store-devtools';
 // Store
 import {reducers, effects, CustomSerializer} from './store';
 import { StoreRouterConnectingModule, RouterStateSerializer } from '@ngrx/router-store';
+
+// Interceptors
+import { JwtInterceptor } from './core/interceptors/jwt.interceptor';
+import { SuccessInterceptor } from './core/interceptors/success.interceptor';
+import { ErrorInterceptor } from './core/interceptors/error.interceptor';
 
 // Components
 import {AppComponent} from './app.component';
@@ -35,6 +41,7 @@ export const metaReducers: MetaReducer<any>[] = [];
     BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
+    LoadingBarHttpClientModule,
     SharedModule,
     MaterialModule,
     StoreModule.forRoot(reducers, {metaReducers}),
@@ -47,6 +54,21 @@ export const metaReducers: MetaReducer<any>[] = [];
     {
       provide: RouterStateSerializer,
       useClass: CustomSerializer
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: JwtInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: SuccessInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ErrorInterceptor,
+      multi: true
     }
   ],
   bootstrap: [ AppComponent ]

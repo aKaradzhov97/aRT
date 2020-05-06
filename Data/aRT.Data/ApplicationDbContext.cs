3e342c1aeb34260fc26b1f292d88486c319b5769
Enes <losthell92@gmail.com>
2020-05-06 16:12:05 +0300

    Updated ApplicationDbContext connection Settings

--- Data/aRT.Data/ApplicationDbContext.cs
+++ Data/aRT.Data/ApplicationDbContext.cs
@@ -22,14 +22,13 @@
         {
         }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-                optionsBuilder.UseMySQL("server=localhost;port=3306;database=aRT;user=root;password=;");
-            }
-        }
-
+        // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        // {
+        //     if (!optionsBuilder.IsConfigured)
+        //     {
+        //         optionsBuilder.UseMySQL("server=localhost;port=3306;database=aRT;user=root;password=;");
+        //     }
+        // }
         public DbSet<Product> Products { get; set; }
 
         public DbSet<UserProduct> UserProducts { get; set; }
